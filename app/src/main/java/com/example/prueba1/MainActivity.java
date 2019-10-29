af11d229a65700c41e1011936ea7db3aefa749d6
Jorge Iza <izajorge7@gmail.com>
2019-10-29 13:22:07 -0500

    Menu Principal (Home) añadido botones Scroll, actividades 13 y actividad 14 realizadas con funcionalidad.

--- app/src/main/java/com/example/prueba1/MainActivity.java
+++ app/src/main/java/com/example/prueba1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.prueba1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void activity13(View view){
+        Intent act13 = new Intent(this, activity_13.class);
+        startActivity(act13);
+    }
+
+    public void activity24(View view){
+        Intent act24 = new Intent(this, activity_24.class);
+        startActivity(act24);
     }
 }
